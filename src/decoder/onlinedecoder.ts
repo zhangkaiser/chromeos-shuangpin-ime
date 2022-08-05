5b23c64a2a9274ff20d3ed9c3c0f65988cbb785d
KaiserZh <ydfyh@qq.com>
2022-08-05 23:30:05 +0800

    Update online decoder.

--- src/decoder/onlinedecoder.ts
+++ src/decoder/onlinedecoder.ts
@@ -1,9 +1,33 @@
 import { IMEResponse } from "./response";
 
+/** Locale test extid */
+const googleInputExtID = "gkckmcceljejgjpphcoepifmhiiogjbg";
+
+/**
+ * @todos layouts, imeconfigs
+ */
+
 export default class OnlineDecoder extends EventTarget implements IDecoder {
   
   constructor(engineID: string) {
     super();
+  }
+
+  #sendMessage(data: any) {
+    return new Promise((resolve: (value: any) => void, reject) => {
+      try {
+        chrome.runtime.sendMessage(googleInputExtID, data, resolve);
+      } catch(e) { 
+        reject(e); 
+      }
+    })
+  }
+
+  #getConfig() {
+    // 1, Current config name.
+    // 2, config name list.
+    // 10, Curent config name.
+    return this.#sendMessage({gp: true})
   }
 
   decode(sourceToken: string, chooseId: number) {
