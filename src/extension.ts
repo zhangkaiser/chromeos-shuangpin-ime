952547781fc618c7e4d9a090889f0e9b90429a93
KaiserZh <ydfyh@qq.com>
2022-09-18 09:00:15 +0800

    vscode test.

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,58 @@
  */
 
 import * as vscode from "vscode";
-import Decoder  from "src/decoder/cdecoder";
+import { Controller } from "src/controller";
+
+const IMECommands = {
+  TOGGLE: "vscode-ime.toggle",
+
+
+}
+
+type IMECommandKey =  keyof typeof IMECommands;
+
+const registerCommand = vscode.commands.registerCommand;
+
+class IMEAdapter {
+  
+  controller = new Controller();
+
+  enabled = false;
+
+  constructor(readonly context: vscode.ExtensionContext) {
+    this.#init();
+  }
+
+  get subscriptions() {
+    return this.context.subscriptions;
+  }
+
+  #init() {
+    // 
+    for (let key in IMECommands) {
+      let commandKey =  key as IMECommandKey;
+      switch(commandKey) {
+        case "TOGGLE":
+          this.subscriptions.push(registerCommand(
+            IMECommands[commandKey], this.toggle.bind(this)
+          ));
+          break;
+      }
+    }
+  }
+
+  toggle() {
+    if (this.enabled) {
+
+    }
+  }
+}
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log("Congraulations, your extension is now active!");
-  let decoder = new Decoder('zh-wasm-shuangpin', 'pinyinjiajia_o');
-  let a = decoder.decode("yidjyidi", -1);
-  console.log(a);
+
+  let ime = new IMEAdapter(context);
+  
+
   const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
     vscode.window.showInformationMessage("Hello World from testing.");
   });
---
+++ src/model/vscodeconfig.ts
@@ -0,0 +1,11 @@
+
+import * as vscode from "vscode";
+
+
+
+export class VscodeConfig {
+
+  constructor() {
+  }
+
+}
